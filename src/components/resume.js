06b2864ea851ba79777c4ed2db7f36babae2d95d
deenobahri12 <deenobahri12@gmail.com>
2020-06-02 11:59:10 -0500

    first commit

--- src/components/resume.js
+++ src/components/resume.js
@@ -90,7 +90,7 @@ class Resume extends Component {
                <h5>BI worldwide:</h5>
                <hr/>
              <p className='descrip'>
-               I started at BI-Worldwide as a software engineer specializing in front end development. 
+               I started at BI-Worldwide as a software engineer specializing in Javascript development. 
                This role was centered in the Product-delivery department and completed projects from beginning to end in the business intelligence industry. 
              </p>
 
